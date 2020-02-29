57d90d0d1161c1379446ecb53f5c454795d79ee6
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2020-02-29 10:25:00 +0100

    add stubs

--- py_client/udp_client.py
+++ py_client/udp_client.py
@@ -23,12 +23,27 @@ def receive_welcome(message):
     print("Computer sagt ja.")
     pass
 
+
+def receive_new_season(data):
+    # Expect: NEW SEASON;$season
+    pass
+
+
+def receive_new_game(data):
+    # Expect: NEW GAME;$opponent
+    pass
+
+
 def udp_listen():
     while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         print("received message:", data)
         if data.startswith(b"WELCOME;"):
             receive_welcome(data)
+        elif data.startswith(b"NEW SEASON;"):
+            receive_new_season(data)
+        elif data.startswith(b"NEW GAME;"):
+            receive_new_game(data)
 
 
 listener = threading.Thread(target=udp_listen)
