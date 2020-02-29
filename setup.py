f2c4a6e4ce2705898989b0157bc459ac65562f60
Thorben Jensen <jensen.thorben@gmail.com>
2020-02-29 09:58:34 +0100

    hello world from python

---
+++ setup.py
@@ -0,0 +1,15 @@
+from glob import glob
+from setuptools import setup, find_packages
+
+
+setup(
+    name="py_client",
+    version="0.1.0",
+    packages=find_packages(),
+    # scripts=glob("bin/*"),
+    install_requires=[
+    ],
+    extras_require={
+        "dev": ["black", "jupyter", "pylama", "rope"],
+    },
+)
