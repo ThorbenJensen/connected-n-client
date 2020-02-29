37af9021190398f0203832d669906f7517882ce7
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2020-02-29 10:10:17 +0100

    remove cruft

--- py_client/udp_client.py
+++ py_client/udp_client.py
@@ -9,11 +9,9 @@ USERNAME = b"botty_asldkfj"
 
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
-# print("message:", MESSAGE)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-# sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 def register(username):
     message = b"REGISTER;" + username
