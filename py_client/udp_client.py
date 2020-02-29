67e577e5c2c13dcca063b181fcbbc76093aba97e
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2020-02-29 10:05:30 +0100

    refactor REGISTER into a method

--- py_client/udp_client.py
+++ py_client/udp_client.py
@@ -6,15 +6,20 @@ UDP_IP = "192.168.1.136"
 UDP_PORT = 4446
 
 USERNAME = b"botty_asldkfj"
-MESSAGE = b"REGISTER;" + USERNAME
 
 print("UDP target IP:", UDP_IP)
 print("UDP target port:", UDP_PORT)
-print("message:", MESSAGE)
+# print("message:", MESSAGE)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+# sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+def register(username):
+    message = b"REGISTER;" + username
+    sock.sendto(message, (UDP_IP, UDP_PORT))
+
+register(USERNAME)
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
