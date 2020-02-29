65dd54323898b133430b38dc558bd82d99d1d603
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2020-02-29 10:16:28 +0100

    refactor listener into a separate thread

--- py_client/udp_client.py
+++ py_client/udp_client.py
@@ -1,6 +1,7 @@
 """ UDP client in python. """
 
 import socket
+import threading
 
 UDP_IP = "192.168.1.136"
 UDP_PORT = 4446
@@ -22,12 +23,15 @@ def receive_welcome(message):
     print("Computer sagt ja.")
     pass
 
+def udp_listen():
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print("received message:", data)
+        if data.startswith(b"WELCOME;"):
+            receive_welcome(data)
+
+
+listener = threading.Thread(target=udp_listen)
+listener.start()
 
 send_register(USERNAME)
-
-
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message:", data)
-    if data.startswith(b"WELCOME;"):
-        receive_welcome(data)
